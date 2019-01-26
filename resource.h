b33323f33ef2e42338923d3017daddc20aab0bf6
TwinFan <twinfan@sonux.net>
2019-01-26 12:54:36 +0100

    Align VC with folder structure, DLL version v0.92

--- resource.h
+++ resource.h
@@ -1,6 +1,7 @@
-//{{NO_DEPENDENCIES}}
+﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by LiveTraffic.rc
+//
 
 // Next default values for new objects
 // 
@@ -8,7 +9,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
