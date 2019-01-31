384021022a06714dfba9ebe817da4b270293fc7e
TwinFan <twinfan@sonux.net>
2019-01-31 23:58:00 +0100

    Changed folder structure and Docker build

---
+++ Include/ACInfoWnd.h
@@ -0,0 +1,103 @@
+//
+//  SettingsUI.h
+//  LiveTraffic
+
+/*
+ * Copyright (c) 2018, Birger Hoppe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef ACInfoWnd_h
+#define ACInfoWnd_h
+
+#include "TFWidgets.h"
+
+//
+// Text Edit field searching for a/c
+//
+
+// Seach for a/c with text provided by user
+// Replace by icao transp hex code
+// Provide LTFlighData/LTAircraft object on request
+class TFACSearchEditWidget : public TFTextFieldWidget
+{
+protected:
+    std::string transpIcao;         // key to the a/c to display
+public:
+    TFACSearchEditWidget (XPWidgetID _me = NULL, const char* szKey = NULL);
+
+    // Find my aircraft
+    const LTFlightData* SearchFlightData (const std::string key);
+    void SetTranspIcao (const std::string transpIcao);
+
+    // Get the found aircraft
+    bool HasTranspIcao () const { return !transpIcao.empty(); }
+    const std::string GetTranspIcao () const { return transpIcao; }
+    unsigned int GetTranspIcaoInt () const { return (unsigned)strtoul (transpIcao.c_str(), NULL, 16); }
+    const LTFlightData* GetFlightData () const;
+    const LTAircraft* GetAircraft () const;
+    
+protected:
+    // capture entry into the key field
+    virtual bool MsgTextFieldChanged (XPWidgetID textWidget, std::string text);
+    
+};
+
+//
+// A/C Info Main Window
+//
+class ACIWnd : public TFMainWindowWidget
+{
+protected:
+    XPWidgetID* widgetIds = nullptr;    // all widget ids in the dialog
+    
+    // edit field for a/c key
+    TFACSearchEditWidget txtAcKey;
+    bool    bAutoAc = false;        // do we pick the a/c to show automatically?
+    
+    // data output fields
+    TFWidget valSquawk;
+    TFWidget valPos, valBearing, valDist, valPhase;
+    TFWidget valGear, valFlaps, valLights;
+    TFWidget valHeading, valPitch, valRoll, valAlt, valAGL, valSpeed, valVSI;
+    
+public:
+    ACIWnd(const char* szKey = nullptr);
+    ACIWnd(bool bAuto) : ACIWnd(bAuto ? INFO_WND_AUTO_AC : nullptr) {}
+    virtual ~ACIWnd();
+    
+    // constructor finished initialization?
+    bool isEnabled () const { return widgetIds && *widgetIds; }
+    
+    // create a new window
+    static ACIWnd* OpenNewWnd (const char* szIcao = nullptr);
+    
+protected:
+    // capture entry into the key field
+    virtual bool MsgTextFieldChanged (XPWidgetID textWidget, std::string text);
+    // triggered every seond to update values in the window
+    virtual bool TfwMsgMain1sTime ();
+    // Updated myself
+    bool UpdateFocusAc ();          // switch to another focus a/c?
+    void UpdateStatValues ();       // static fields
+    void UpdateDynValues ();        // dynamic fields
+};
+
+#endif /* ACInfoWnd_h */
---
+++ Include/SettingsUI.h
@@ -0,0 +1,122 @@
+//
+//  SettingsUI.h
+//  LiveTraffic
+
+/*
+ * Copyright (c) 2018, Birger Hoppe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef SettingsUI_h
+#define SettingsUI_h
+
+#include "ACInfoWnd.h"
+
+// Shows simDate/time, updated every second
+class LTCapDateTime : public TFTextFieldWidget
+{
+protected:
+    TFDataRefLink simDate, simTime;
+public:
+    LTCapDateTime (XPWidgetID _me = NULL);
+    void SetCaption ();
+protected:
+    virtual bool TfwMsgMain1sTime ();
+    virtual bool MsgTextFieldChanged (XPWidgetID textWidget, std::string text);
+};
+
+//
+// Settings UI Main window
+//
+class LTSettingsUI : public TFMainWindowWidget
+{
+protected:
+    XPWidgetID* widgetIds;              // all widget ids in the dialog
+    TFButtonGroup tabGrp;               // button group to switch 'tabs'
+    // sub-windows ('tabs')
+    TFWidget subBasicsLive, subBasicsHistoric, subAcLabel, subAdvcd, subCSL, subDebug;
+    
+    // Basics tab
+    TFButtonDataRef btnBasicsEnable,    // enable display of aircrafts
+                    btnBasicsAutoStart,
+                    btnBasicsHistoric;
+    // enable/disable flight data channels
+    TFButtonDataRef btnOpenSkyLive, btnOpenSkyMasterdata, btnADSBLive, btnADSBHistoric;
+    LTCapDateTime txtDateTime;          // display/edit current simtime
+    
+    // A/C Labels tab
+    TFDataRefLink drCfgLabels;          // links to dataRef livetraffic/cfg/labels
+    TFDataRefLink drCfgLabelShow;       // links to dataRef livetraffic/cfg/label_show
+    TFButtonGroup btnGrpLabelColorDyn;
+    TFDataRefLink drLabelColDyn;
+    TFIntFieldDataRef intLabelColor;
+
+    // Advanced tab
+    TFButtonGroup logLevelGrp;          // radio buttons to select logging level
+    TFIntFieldDataRef intMaxNumAc, intMaxFullNumAc, intFullDistance;
+    TFIntFieldDataRef intFdStdDistance, intFdRefreshIntvl;
+    TFIntFieldDataRef intFdBufPeriod, intAcOutdatedIntvl;
+    TFButtonDataRef btnAdvcdLndLightsTaxi;
+    
+    // CSL tab
+    enum { SETUI_CSL_PATHS=7, SETUI_CSL_ELEMS_PER_PATH=3 };
+    static constexpr int SETUI_CSL_PATHS_NUM_ELEMS = SETUI_CSL_PATHS * SETUI_CSL_ELEMS_PER_PATH;
+    TFTextFieldWidget txtCSLPaths[SETUI_CSL_PATHS];
+    TFTextFieldWidget txtDefaultAcType, txtGroundVehicleType;
+    
+    // Debug tab
+    TFACSearchEditWidget txtDebugFilter;
+    TFButtonDataRef btnDebugLogACPos, btnDebugLogModelMatch, btnDebugLogRawFd;
+    TFTextFieldWidget txtFixAcType, txtFixOp, txtFixLivery;
+
+public:
+    LTSettingsUI();
+    ~LTSettingsUI();
+    
+    // (de)register widgets (not in constructor to be able to use global variable)
+    void Enable();
+    void Disable();
+    bool isEnabled () const { return widgetIds && *widgetIds; }
+    
+    // first creates the structure, then shows the window
+    virtual void Show (bool bShow = true);
+
+protected:
+    // capture entry into 'filter for transponder hex code' field
+    virtual bool MsgTextFieldChanged (XPWidgetID textWidget, std::string text);
+    // writes current values out into config file
+    virtual bool MsgHidden (XPWidgetID hiddenWidget);
+
+    // update state of log-level buttons from dataRef
+    virtual bool TfwMsgMain1sTime ();
+
+    // handles show/hide of 'tabs'
+    virtual bool MsgButtonStateChanged (XPWidgetID buttonWidget, bool bNowChecked);
+    virtual bool MsgPushButtonPressed (XPWidgetID buttonWidget);
+    
+    // Handle checkboxes for a/c labels
+    void LabelBtnInit();
+    void LabelBtnSave();
+    
+    // Save CSL path / Load a CSL package
+    void SaveCSLPath(int idx);
+};
+
+#endif /* SettingsUI_h */
