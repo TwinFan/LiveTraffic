34814628a9d56a670e844eb30b6c30eba56a067c
TwinFan <twinfan@sonux.net>
2020-05-01 22:18:47 +0200

    v2.01

--- Include/Constants.h
+++ Include/Constants.h
@@ -29,7 +29,7 @@
 //
 // MARK: Version Information (CHANGE VERSION HERE)
 //
-constexpr float VERSION_NR = 2.00f;
+constexpr float VERSION_NR = 2.01f;
 constexpr bool VERSION_BETA = true;
 extern float verXPlaneOrg;          // version on X-Plane.org
 extern int verDateXPlaneOrg;        // and its date
